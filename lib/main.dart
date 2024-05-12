2026cbf684ec855fbb2b69aa153eda5df73f4012
Birbal Kumar <birbalkr1435@gmail.com>
2024-05-12 23:47:53 +0530

    scrollview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -33,33 +34,39 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: InkWell(
-        onTap: (){
-          print("tap");
-        },
-        onDoubleTap: (){
-          print("doubletap");
-        },
-        onLongPress: (){
-          print("long press");
-        },
-        child: Container(
-          child: Center(
-            child: InkWell(
-              onTap:(){
-                print("text");
-              } ,
-              child: Text("Text"),
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 11),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(width: 200, height: 200, color: Colors.blue),
+                        Container(width: 200, height: 200, color: Colors.red),
+                        Container(width: 200, height: 200, color: Colors.green),
+                        Container(width: 200, height: 200, color: Colors.black),
+                        Container(width: 200, height: 200, color: Colors.orange),
+                        Container(width: 200, height: 200, color: Colors.yellow)
+                      ],
+                    ),
+                  ),
+                ),
+                Container(height: 200, color: Colors.blue),
+                Container(height: 200, color: Colors.red),
+                Container(height: 200, color: Colors.green),
+                Container(height: 200, color: Colors.black),
+                Container(height: 200, color: Colors.orange),
+                Container(height: 200, color: Colors.yellow)
+              ],
             ),
           ),
-        width: 100,
-        height: 100,
-        color: Color.fromARGB(255, 216, 25, 25),
-      ),
-      ),
-    );
+        ));
   }
 }
